ac443643b641b32a4ccfa7bf9589d200d009161a
luyang <luyangborn@126.com>
2018-07-07 08:53:26 +0800

    title更改

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -2,7 +2,7 @@ import allure,pytest
 
 class Test_001:
     @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
-    @allure.step(titile='第一个测试')
+    @allure.step(title='第一个测试')
     def test_001_1(self):
         allure.attach("这是一个描述","我是具体内容")
         assert 0
