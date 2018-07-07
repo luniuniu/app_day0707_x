d2e08b11164d91aed821fe97c98904cf4bfa1b27
luyang <luyangborn@126.com>
2018-07-07 08:44:27 +0800

    添加严重级别

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,7 +1,7 @@
-import allure
+import allure,pytest
 
 class Test_001:
-
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(titile='第一个测试')
     def test_001_1(self):
         allure.attach("这是一个描述","我是具体内容")
